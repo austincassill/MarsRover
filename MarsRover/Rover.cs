a89ad5016b170fa2dc8c512f15a7ae61d7fb3b81
austincassill <auscassill@gmail.com>
2022-09-28 16:40:22 -0400

    refactoring and validation changes

--- MarsRover/Rover.cs
+++ MarsRover/Rover.cs
@@ -1,45 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using MarsRover.Interfaces;
 
 namespace MarsRover
 {
     public class Rover : IRover
     {
-        private static readonly string[] ValidDirections = {"N", "S", "E", "W"};
-        private readonly IPlateau _plateau;
+        private static readonly string[] ValidDirections = { "N", "S", "E", "W" };
         private readonly IConsoleWrapper _console;
-        private int _longitude;
-        private int _latitude;
+        private readonly IPlateau _plateau;
         private string _direction;
+        private int _latitude;
+        private int _longitude;
+
         public Rover(IPlateau plateau, IConsoleWrapper console)
         {
             _plateau = plateau;
             _console = console;
         }
 
-        public void DeployRover()
+        public void DeploymentValidation()
         {
             while (true)
             {
                 _console.WriteLine("Please enter starting longitude, latitude, and direction. (f.e. 3 4 N)");
                 var input = _console.ReadLine().Split(' ');
-                var longitude = int.Parse(input[0]);
-                var latitude = int.Parse(input[1]);
-                if (_plateau.ValidCoordinates(longitude, latitude) && ValidDirections.Contains(input[2]))
+
+                if (!int.TryParse(input[0], out var longitude))
                 {
-                    _longitude = longitude;
-                    _latitude = latitude;
-                    _direction = input[2];
-                    break;
+                    _console.WriteLine("Longitude is invalid. Must be an integer.");
+                    continue;
                 }
-                else
+
+                if (!int.TryParse(input[1], out var latitude))
                 {
-                    _console.WriteLine("Invalid Input.");
+                    _console.WriteLine("Latitude is invalid. Must be an integer.");
+                    continue;
                 }
+
+                if (!ValidDirections.Contains(input[2]))
+                    _console.WriteLine("Direction must be N (north), S (south), E (east), or W (west).");
+
+                if (!_plateau.ValidCoordinates(longitude, latitude))
+                {
+                    _console.WriteLine("Rover location is outside the bounds of the plateau.");
+                    continue;
+                }
+
+                _longitude = longitude;
+                _latitude = latitude;
+                _direction = input[2];
+                break;
             }
         }
     }
